86ba26260766cd6b0d098644f6e9e67890a85a16
Thomas Kerbl <34186142+Dementophobia@users.noreply.github.com>
2020-12-05 09:16:31 +0100

    Solution Day 5

---
+++ 2020_05_p2.py
@@ -0,0 +1,35 @@
+from aoc import read_file, timer
+
+def calc_seat_id(seat):
+    row_low, row_high = 0, 127
+    col_low, col_high = 0, 7
+        
+    for char in seat[:7]:
+        if char == "B":
+            row_low  += (row_high - row_low + 1) // 2
+        else:
+            row_high -= (row_high - row_low + 1) // 2
+
+    for char in seat[7:]:
+        if char == "R":
+            col_low  += (col_high - col_low + 1) // 2
+        else:
+            col_high -= (col_high - col_low + 1) // 2
+    
+    return row_low * 8 + col_low
+
+@timer
+def solve():
+    input = read_file("05")
+
+    seats = sorted(input, key = lambda s: s[7:])
+    seats = sorted(seats, key = lambda s: s[:7], reverse = True)
+
+    for i in range(len(seats)):
+        current_id = calc_seat_id(seats[i])
+        if i and prev_id + 2 == current_id:
+            return prev_id + 1
+        prev_id = current_id
+
+result = solve()
+print(f"Solution: {result}")
